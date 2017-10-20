6c0403d6b0953cd307264b87864cbaadb5f1ebc4
ruffoa <ruffo.alex@gmail.com>
2017-10-20 16:13:07 -0400

    Added comments to some of the Dibs stuff

--- QLife/app/src/main/java/engsoc/qlife/ui/fragments/ILCRoomInfoFragment.java
+++ QLife/app/src/main/java/engsoc/qlife/ui/fragments/ILCRoomInfoFragment.java
@@ -222,21 +222,21 @@ public class ILCRoomInfoFragment extends Fragment {
         mNavView.getMenu().findItem(R.id.nav_rooms).setChecked(true);
     }
 
-    private void onFABClick() {
+    private void onFABClick() {     // on a click, we organize the ILC rooms from small to large, plus 1 "unknown" (aka rm 111)
         ArrayList<DataObject> small = new ArrayList<DataObject>();
         ArrayList<DataObject> med = new ArrayList<DataObject>();
-        ArrayList<DataObject> large = new ArrayList<DataObject>();
+        ArrayList<DataObject> large = new ArrayList<DataObject>();  // set up an arrayList to hold all of the rooms for each category
         ArrayList<DataObject> other = new ArrayList<DataObject>();
         ArrayList<DataObject> res = new ArrayList<DataObject>();
 
         for (DataObject obj : mRoomData) {
-            if (Pattern.compile(Pattern.quote("small"), Pattern.CASE_INSENSITIVE).matcher(obj.getmText2()).find())
+            if (Pattern.compile(Pattern.quote("small"), Pattern.CASE_INSENSITIVE).matcher(obj.getmText2()).find())  // essentially, this just means if the room description (obj.getmText2 (yay variable names :) ) contains the word "small", we add it to the list of small rooms
                 small.add(obj);
-            else if (Pattern.compile(Pattern.quote("medium"), Pattern.CASE_INSENSITIVE).matcher(obj.getmText2()).find())
+            else if (Pattern.compile(Pattern.quote("medium"), Pattern.CASE_INSENSITIVE).matcher(obj.getmText2()).find())    // ditto for the other two types
                 med.add(obj);
             else if (Pattern.compile(Pattern.quote("large"), Pattern.CASE_INSENSITIVE).matcher(obj.getmText2()).find())
                 large.add(obj);
-            else other.add(obj);
+            else other.add(obj);    // it's not a small, medium or large room, so it *must* be rm 111 (at least until they change things)
         }
 
 
@@ -269,28 +269,28 @@ public class ILCRoomInfoFragment extends Fragment {
         });
     }
 
-    private boolean onFABLongClick() {
+    private boolean onFABLongClick() {  // for this function, we grab all rooms that are currently unbooked, and display a nice list of them
 
-        mProgressView.setVisibility(View.VISIBLE);
+        mProgressView.setVisibility(View.VISIBLE);  // show a loading screen while this is going on, it takes a good 3-5 seconds depending on your internet connection, and that's with a SD650, which is a mid-high end CPU -> ToDo: make this more efficient if possible, although since network calls take a while, and we need to use a new call for every room ,it may not be possible to make this faster
 
-        ILCRoomObjManager roomInf = new ILCRoomObjManager(this.getContext());
-        ArrayList<DataObject> result = new ArrayList<DataObject>();
-        ArrayList<DatabaseRow> data = roomInf.getTable();
+        ILCRoomObjManager roomInf = new ILCRoomObjManager(this.getContext());   // get the database table containing the list of all the ILC rooms in the database
+        ArrayList<DataObject> result = new ArrayList<DataObject>();                 // create a arratList to hold the rooms that are avaliable
+        ArrayList<DatabaseRow> data = roomInf.getTable();                   // get the data from the roomInfo table in the database, and put it in a arrayList
 
-        Calendar cal = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();  // create a new calendar, and initialize it to today
 
         try {
-            if (data != null && data.size() > 0) {
+            if (data != null && data.size() > 0) {  // if there exists a room in the table (there should be some, unless this is the first launch, and the table has not been populated yet)
 
-                showProgress(true);
-                mProgressView.setVisibility(View.VISIBLE);
+                showProgress(true);                 // display the loading scree -> ToDo: make this actually work :P
+                mProgressView.setVisibility(View.VISIBLE);   // display the loading scree -> ToDo: make this actually work :P
 
-                for (DatabaseRow row : data) {
-                    getDibsRoomInfo dibs = new getDibsRoomInfo(this.getContext());
-                    ILCRoomObj room = (ILCRoomObj) row;
-                    roomAvaliabiliy = dibs.execute(room.getRoomId(), cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH), cal.get(Calendar.YEAR)).get();
-                    int status = getDayAvaliability();
-                    if (status == 0) {
+                for (DatabaseRow row : data) {  // for each room within the database
+                    getDibsRoomInfo dibs = new getDibsRoomInfo(this.getContext());  // create a new instance of the getDibsRoomInfo class, which essentially uses the d!bs API to get a JSON with room booking data for the given room at a given date
+                    ILCRoomObj room = (ILCRoomObj) row; // create a new ILC Room Object
+                    roomAvaliabiliy = dibs.execute(room.getRoomId(), cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH), cal.get(Calendar.YEAR)).get();    // get the room avaliability on the UI thread (this means it's slllloooooooowwwwwwwwww)
+                    int status = getDayAvaliability();  // see if the room is free today / right now
+                    if (status == 0) {                  // room is free right now for the current hour timeslot
                         result.add(new DataObject(room.getName(), "Is Avaliable Now", room.getRoomId(), true, "", room.getDescription()));
 
                     } else if (status == 2) // if the room was booked for the current time slot, but the slot is almost over (sHour == now - 1 && nowMin < 30)
@@ -340,7 +340,7 @@ public class ILCRoomInfoFragment extends Fragment {
             for (DatabaseRow row : data) {
                 ILCRoomObj room = (ILCRoomObj) row;
                 boolean hasTV = room.getDescription().contains("TV") || room.getDescription().contains("Projector") ? true : false;
-                result.add(new DataObject(room.getName(), room.getDescription(), room.getRoomId(), hasTV, ""));
+                result.add(new DataObject(room.getName(), room.getDescription(), room.getRoomId(), hasTV, "")); // create a new DataObject with the inputed parameters to be shown within the recyclerView
             }
             mRoomData = result;
             return result;
@@ -348,41 +348,41 @@ public class ILCRoomInfoFragment extends Fragment {
         return null;
     }
 
-    public int getDayAvaliability() {
+    public int getDayAvaliability() {   // gets the information sent to it, and returns whether or not the room is free at the current timeslot
 
 
-        if (roomAvaliabiliy != null && roomAvaliabiliy.length() > 0) {
+        if (roomAvaliabiliy != null && roomAvaliabiliy.length() > 0) {  // if the JSON array containing the data is not null (which would be bad)
             try {
-                JSONArray arr = new JSONArray(roomAvaliabiliy);
+                JSONArray arr = new JSONArray(roomAvaliabiliy); // make a JSON array variable to hold a properly formatted JSON object
 
                 int state = 0;
 
-                Calendar cal = Calendar.getInstance();
-                int now = cal.get(Calendar.HOUR_OF_DAY);
-                int nowMin = cal.get(Calendar.MINUTE);
+                Calendar cal = Calendar.getInstance();      // get a new calendar set to the current time
+                int now = cal.get(Calendar.HOUR_OF_DAY);    // get the current hour in 24 hour format (it's a lot easier like this)
+                int nowMin = cal.get(Calendar.MINUTE);      // get the current minute, for checking on the half hour purposes (because aligning everything to a hour start would have been too easy)
 
-                for (int i = 0; i < arr.length(); i++) {
-                    JSONObject roomInfo = arr.getJSONObject(i);
-                    String start = roomInfo.getString("StartTime");
+                for (int i = 0; i < arr.length(); i++) {    // for all of the data within the array
+                    JSONObject roomInfo = arr.getJSONObject(i); // get the info for the JSON object (each booking is it's own object)
+                    String start = roomInfo.getString("StartTime"); // get the startTime and endTime contained within the object
                     String end = roomInfo.getString("EndTime");
 
-                    start = start.substring(start.indexOf("T") + 1);
+                    start = start.substring(start.indexOf("T") + 1);    // get the posistion of the start and end times, and set the string to be the useful part
                     end = end.substring(end.indexOf("T") + 1);
 
-                    int sHour = Integer.parseInt(start.substring(0, 2));
-                    int eHour = Integer.parseInt(start.substring(0, 2));
+                    int sHour = Integer.parseInt(start.substring(0, 2));    // cast the starting hour to an int
+                    int eHour = Integer.parseInt(start.substring(0, 2));    // cast the ending hour to an int
 //                    if (sHour == now && nowMin < 30)            // if the room was booked for the current timeslot, but it is still before the timeslot starts
 //                        return 4;
-                    if (sHour == now || (sHour <= now && now <= eHour)) {
-                        return 1;
+                    if (sHour == now || (sHour <= now && now <= eHour)) {   // if the current hour is equal to the start hour of a booking, or the current hour is within the time of a booking
+                        return 1;       // return 1, the room is currently booked, so we are done, return immediately.
                     } else if (sHour == now - 1 && nowMin < 30) // if the room was booked for the current time slot, but the slot is almost over
-                        state = 2;
+                        state = 2;  // keep state as 2, so that people know that the room is free for the next hour block.  If return 1 is never called, 2 will be returned
                     else if (sHour == now + 1)                  // if the room is booked for the next time slot
-                        state = 3;
+                        state = 3;  // let users know that the room will be booked in the next slot, so that they know someone will be after them
                 }
-                return state;
-            } catch (JSONException e) {
-
+                return state;   // if the room is not currently booked, return the state of the room booking as explained above
+            } catch (JSONException e) { // something broke :(
+                
             }
         }
         return 0;
