3fdc235ac87c143ae4d9660f2a4a55261b7b33c7
Carson Cook <cook.carson38@gmail.com>
2017-11-26 17:12:09 -0500

    cleanup ParseICS

--- QLife/app/src/main/java/engsoc/qlife/ICS/ParseICS.java
+++ QLife/app/src/main/java/engsoc/qlife/ICS/ParseICS.java
@@ -1,17 +1,7 @@
 package engsoc.qlife.ICS;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.util.Log;
-
-import engsoc.qlife.database.local.DatabaseRow;
-import engsoc.qlife.database.local.courses.Course.Course;
-import engsoc.qlife.database.local.courses.Course.CourseManager;
-import engsoc.qlife.database.local.courses.OneClass.OneClass;
-import engsoc.qlife.database.local.courses.OneClass.OneClassManager;
-import engsoc.qlife.database.local.users.User;
-import engsoc.qlife.database.local.users.UserManager;
-import engsoc.qlife.ui.fragments.StudentToolsFragment;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -28,13 +18,22 @@ import java.util.Locale;
 import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
+import engsoc.qlife.database.local.DatabaseRow;
+import engsoc.qlife.database.local.courses.Course.Course;
+import engsoc.qlife.database.local.courses.Course.CourseManager;
+import engsoc.qlife.database.local.courses.OneClass.OneClass;
+import engsoc.qlife.database.local.courses.OneClass.OneClassManager;
+import engsoc.qlife.database.local.users.User;
+import engsoc.qlife.database.local.users.UserManager;
+import engsoc.qlife.ui.fragments.StudentToolsFragment;
+import engsoc.qlife.utility.Constants;
+
 /**
  * Created by Alex on 1/18/2017.
  * Class to parse the ICS file.
  */
-public class ParseICS {
-
-    private String TAG = StudentToolsFragment.class.getSimpleName();
+class ParseICS {
+    private final String TAG = StudentToolsFragment.class.getSimpleName();
     private OneClassManager mOneClassManager;
     private CourseManager mCourseManager;
 
@@ -44,97 +43,50 @@ public class ParseICS {
         this.mContext = context;
     }
 
-    public List<String> readLine(String path) {
+    private List<String> readDownloadFile() {
         List<String> mLines = new ArrayList<>();
-        AssetManager am = mContext.getAssets();
-
         try {
-            InputStream is = am.open(path);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line;
-
-            while ((line = reader.readLine()) != null)
-                mLines.add(line);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return mLines;
-    }
-
-    public List<String> readDownloadFile(String path) {
-        List<String> mLines = new ArrayList<>();
-
-        String ret = "";
-
-        try {
-            InputStream inputStream = mContext.openFileInput(path);
+            InputStream inputStream = mContext.openFileInput(Constants.CALENDAR_FILE);
 
             if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder(); //TODO what is stringbuilder used for
+                String receiveString;
 
                 while ((receiveString = bufferedReader.readLine()) != null) {
                     mLines.add(receiveString);
                 }
 
                 inputStream.close();
-                ret = stringBuilder.toString();
             }
         } catch (FileNotFoundException e) {
             Log.e(TAG, "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e(TAG, "Can not read file: " + e.toString());
         }
-        Log.e(TAG, "Done: " + ret);
         return mLines;
     }
 
-    public void parseICSData() {
+    void parseICSData() {
         UserManager mUserManager = new UserManager(this.mContext);
         ArrayList<DatabaseRow> userTable = mUserManager.getTable();
         mOneClassManager = new OneClassManager(mContext);
         mCourseManager = new CourseManager(mContext);
 
-        boolean isInit = true;
-
-        // see if user has initialized database yet, and if the database is up to date
-        /*for (int i = 0; i < user.size(); i++) {
-
-            if (!user.get(i).getDateInit().isEmpty()) {
-                String rTime = user.get(i).getDateInit();
-                int yr = Integer.parseInt(rTime.substring(0, 4));
-                int mon = Integer.parseInt(rTime.substring(5, 7));
-                int day = Integer.parseInt(rTime.substring(8, 10));
-
-                Calendar c = Calendar.getInstance();
-                c.set(yr, mon, day);
-                c.add(Calendar.DATE, 7);
-                Calendar endDate = Calendar.getInstance();
-
-                if (c.after(endDate)) {
-                    isInit = true;
-                }
-            }
-        }*/
-
-        if (mOneClassManager.getTable().isEmpty() || !isInit) {
+        if (mOneClassManager.getTable().isEmpty()) {
             mOneClassManager.deleteTable(OneClass.TABLE_NAME);
 
             boolean isEvent = false;
-            String sTime = "", eTime = "", loc = "", name = "", rTime = "";
+            String loc = "", name = "";
             int hour = 0, minute = 0, year = 0;
             int shour = 0, sminute = 0, sday = 0, smonth = 0;
             boolean repeatWeekly = false;
             String rDayStr = "", rMonStr = "", rYrStr = "";
 
-            List<String> lines = readDownloadFile("cal.ics");
-
+            List<String> lines = readDownloadFile();
             int test = 1;
 
             for (String string : lines) {
-
                 if (string.contains("BEGIN:VEVENT")) {
                     isEvent = true;
                 } else if (string.contains("END:VEVENT")) {
@@ -177,7 +129,6 @@ public class ParseICS {
 
                         Date endDate = cal2.getTime();
                         Date date1;
-
                         cal.add(Calendar.DATE, 7);
                         date1 = cal.getTime();
 
@@ -196,14 +147,13 @@ public class ParseICS {
                         }
                     }
                     repeatWeekly = false;
-                    test += 1;
+                    test++;
 
                 } else if (string.contains(("RRULE:FREQ=WEEKLY;"))) {
                     repeatWeekly = true;
 
                     if (string.contains("UNTIL=")) {
-                        rTime = string.replaceAll("[^0-9]", "");
-
+                        String rTime = string.replaceAll("[^0-9]", "");
                         rDayStr = rTime.substring(6, 8);
                         rMonStr = rTime.substring(4, 6);
                         rYrStr = rTime.substring(0, 4);
@@ -213,14 +163,14 @@ public class ParseICS {
                     if (string.contains("LOCATION"))
                         loc = string.substring(9);
                     else if (string.contains("DTSTART")) {
-                        sTime = string.replaceAll("[^0-9]", "");
+                        String sTime = string.replaceAll("[^0-9]", "");
                         shour = Integer.parseInt(sTime.substring(8, 10));
                         sminute = Integer.parseInt(sTime.substring(10, 12));
                         sday = Integer.parseInt(sTime.substring(6, 8));
                         smonth = Integer.parseInt(sTime.substring(4, 6));
                         year = Integer.parseInt(sTime.substring(0, 4));
                     } else if (string.contains("DTEND")) {
-                        eTime = string.replaceAll("[^0-9]", "");
+                        String eTime = string.replaceAll("[^0-9]", "");
                         hour = Integer.parseInt(eTime.substring(8, 10));
                         minute = Integer.parseInt(eTime.substring(10, 12));
 
@@ -244,7 +194,7 @@ public class ParseICS {
         }
     }
 
-    public void addClassName(String htmlRes, String classType) {
+    private void addClassName(String htmlRes, String classType) {
         if (htmlRes == null || htmlRes.length() == 0)
             return;
 
@@ -253,40 +203,35 @@ public class ParseICS {
 
         for (DatabaseRow course : courses) {
             Course c = (Course) course;
-            Course backup = c;
-            if (c.getTitle().contains(classType)) {
+            if (c.getTitle().contains(classType) && htmlRes.contains(c.getTitle())) {
+                int index = htmlRes.indexOf(c.getTitle());
+                String temp = htmlRes.substring(index);
+                temp = temp.substring(0, temp.indexOf("|"));
+                Course backup = c;
 
-                if (htmlRes.contains(c.getTitle())) {
-                    int index = htmlRes.indexOf(c.getTitle());
-                    String temp = htmlRes.substring(index);
-                    temp = temp.substring(0, temp.indexOf("|"));
-                    c.setTitle(temp);
-                    c.setDescription("true");
-                    mCourseManager.updateRow(backup,c);
-                }
+                c.setTitle(temp);
+                c.setDescription("true");
+                mCourseManager.updateRow(backup, c);
             }
         }
     }
 
-    public void getClassTypes() {
-
+    void getClassTypes() {
         mOneClassManager = new OneClassManager(mContext);
         mCourseManager = new CourseManager(mContext);
         ArrayList<DatabaseRow> courses = mCourseManager.getTable();
-        ArrayList<DatabaseRow> classes = mOneClassManager.getTable();
+        ArrayList<String> types = new ArrayList<>();
 
-
-        String types = "";
         for (DatabaseRow data : courses) {
             Course c = (Course) data;
             String temp = c.getTitle().substring(0, c.getTitle().indexOf(" "));
-            if (!types.contains(temp) && c.getDesription() != "true")
-                types += " " + temp;
+            if (!types.contains(temp) && !("true".equals(c.getDesription())))
+                //string builder not used, as string becomes too long for the builder
+                types.add(temp);
         }
 
-        if (types != "") {
-            String[] parts = types.split(" ");
-            for (final String str : parts) {
+        if (!types.isEmpty()) {
+            for (final String str : types) {
                 if (str.length() > 0) {
                     GetCourseInfo cInfo = new GetCourseInfo() {
                         @Override
@@ -299,5 +244,4 @@ public class ParseICS {
             }
         }
     }
-
 }
---
+++ QLife/app/src/main/java/engsoc/qlife/utility/Constants.java
@@ -0,0 +1,9 @@
+package engsoc.qlife.utility;
+
+/**
+ * Created by Carson on 2017-11-26.
+ * Static class for constants.
+ */
+public class Constants {
+    public static final String CALENDAR_FILE = "cal.ics";
+}
