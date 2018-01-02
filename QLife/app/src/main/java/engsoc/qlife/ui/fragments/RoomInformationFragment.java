e6ac1bc11a84899beff59ffd337cd35118b19824
Carson Cook <cook.carson38@gmail.com>
2018-01-01 21:53:59 -0500

    improved added availability times

--- QLife/app/src/main/java/engsoc/qlife/ui/fragments/RoomInformationFragment.java
+++ QLife/app/src/main/java/engsoc/qlife/ui/fragments/RoomInformationFragment.java
@@ -6,18 +6,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -59,7 +58,7 @@ public class RoomInformationFragment extends Fragment {
         }
 
         mView = inflater.inflate(R.layout.fragment_room_avaliability_info, container, false);
-        setTable();
+        setAvailableTimes();
         return mView;
     }
 
@@ -95,8 +94,10 @@ public class RoomInformationFragment extends Fragment {
         }
     }
 
-    private void setTable() {
-        TableLayout table = mView.findViewById(R.id.availability_table);
+    /**
+     * Adds the available room times to the fragment.
+     */
+    private void setAvailableTimes() {
         ArrayList<DataObject> availability = getDayAvailability();
         if (availability == null || availability.isEmpty()) {
             mView.findViewById(R.id.NoAvailability).setVisibility(View.VISIBLE);
@@ -108,12 +109,7 @@ public class RoomInformationFragment extends Fragment {
 
             if (availability.size() == 1) {
                 //only one slot, don't try to combine multiple
-                TextView textView = new TextView(getContext());
-                String text = firstStart + " " + firstEnd;
-                textView.setText(text);
-                TableRow newRow = new TableRow(getContext());
-                newRow.addView(textView);
-                table.addView(newRow);
+                addAvailableTime(firstStart, firstEnd);
             } else {
                 //try to combine overlapping available slots
                 String start = firstStart;
@@ -121,35 +117,48 @@ public class RoomInformationFragment extends Fragment {
                 for (int i = 1; i < availability.size(); i++) {
                     String nextStart = availability.get(i).getmText1();
                     String nextEnd = availability.get(i).getmText2();
-                    if (!sameTime(end, nextStart)) {
-                        //don't overlap, add start/end and move along
-                        TextView textView = new TextView(getContext());
-                        String text = start + " " + end;
-                        textView.setText(text);
-                        TableRow newRow = new TableRow(getContext());
-                        newRow.addView(textView);
-                        table.addView(newRow);
-                        start = nextStart;
+                    if (isSameTime(end, nextStart)) {
+                        //overlap, so don't add row and move along
                         end = nextEnd;
                     } else {
-                        //overlap, so don't add row and move along
+                        //don't overlap, add start/end and move along
+                        addAvailableTime(start, end);
+                        start = nextStart;
                         end = nextEnd;
                     }
                     if (i == availability.size() - 1) {
                         //at end of loop, print out current slot (next start/end)
-                        TextView textView = new TextView(getContext());
-                        String text = start + " " + end;
-                        textView.setText(text);
-                        TableRow newRow = new TableRow(getContext());
-                        newRow.addView(textView);
-                        table.addView(newRow);
+                        addAvailableTime(start, end);
                     }
                 }
             }
         }
     }
 
-    private boolean sameTime(String s1, String s2) {
+    /**
+     * Function that adds an available time to the fragment view.
+     *
+     * @param start The starting time string.
+     * @param end   The ending time string.
+     */
+    private void addAvailableTime(String start, String end) {
+        LinearLayout layout = mView.findViewById(R.id.room_availability);
+        TextView textView = new TextView(getContext());
+        String text = start + " - " + end;
+        textView.setText(text);
+        textView.setGravity(Gravity.CENTER);
+        layout.addView(textView);
+    }
+
+    /**
+     * Function that determines if two availability Strings have the same time.
+     * The availability strings have From or To with them, so this is needed.
+     *
+     * @param s1 String one. Usually the starting time.
+     * @param s2 String two. Usually the ending time.
+     * @return True if the two have the same time, else false.
+     */
+    private boolean isSameTime(String s1, String s2) {
         //times are after the first space, so check they are equal after the first space
         s1 = s1.substring(s1.indexOf(' ') + 1);
         s2 = s2.substring(s2.indexOf(' ') + 1);
@@ -165,7 +174,7 @@ public class RoomInformationFragment extends Fragment {
     private ArrayList<DataObject> getDayAvailability() {
         ArrayList<DataObject> result = new ArrayList<>();
         for (int i = 0; i < 16; i++) {
-            result.add(new DataObject("From: " + (i + 7) + ":" + 30, "To: " + (i + 8) + ":" + 30));
+            result.add(new DataObject((i + 7) + ":" + 30, (i + 8) + ":" + 30));
         }
 
         if (roomAvailability != null && roomAvailability.length() > 0) {
@@ -197,16 +206,25 @@ public class RoomInformationFragment extends Fragment {
                 }
 
                 for (int j = 0; j < result.size(); j++) {
-                    String test = result.get(j).getmText1().substring(6);
-                    int tempTime = Integer.parseInt(test.substring(0, test.indexOf(":")));
+                    //get hour of start time - use to set AM/PM
+                    String startTime = result.get(j).getmText1();
+                    int tempTime = Integer.parseInt(startTime.substring(0, startTime.indexOf(":")));
 
+                    //set start/end time with PM
                     if (tempTime > 12) {
-                        result.get(j).setmText1("From: " + (tempTime - 12) + ":30 PM");
+                        result.get(j).setmText1((tempTime - 12) + ":30 PM");
                     }
                     if (tempTime >= 12) {
-                        test = result.get(j).getmText2().substring(4);
-                        int endTime = Integer.parseInt(test.substring(0, test.indexOf(":")));
-                        result.get(j).setmText2("To: " + (endTime - 12) + ":30 PM");
+                        startTime = result.get(j).getmText2();
+                        int endTime = Integer.parseInt(startTime.substring(0, startTime.indexOf(":")));
+                        result.get(j).setmText2((endTime - 12) + ":30 PM");
+                    }
+                    //set start/end time with AM
+                    if (tempTime <= 11) {
+                        result.get(j).setmText1(startTime + " AM");
+                    }
+                    if (tempTime < 11) {
+                        result.get(j).setmText2(result.get(j).getmText2() + " AM");
                     }
                 }
                 return result;
