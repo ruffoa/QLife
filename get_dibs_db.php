c2fdc84ea1242763ea7cf1cf9134602ad8832388
Carson Cook <cook.carson38@gmail.com>
2018-04-18 21:10:28 -0400

    php script to send dibs data to db

---
+++ get_dibs_db.php
@@ -0,0 +1,15 @@
+<?php
+    //connect to DB
+    require_once __DIR__ . '/db_connect.php';
+    $db = connectDatabase();
+
+    for ($i=0; $i<100; $i++) { //<100 so future room IDs are in loop - need update if more rooms come - break when hit room with API call ID
+        $year=date("Y");
+        $month=date("n");
+        $day=date("j");
+        $bookings=file_get_contents("https://queensu.evanced.info/dibsAPI/reservations/$year-$month-$day/$i");
+        if ($bookings!="[]"){ //bad i values, not room IDs
+            $res=mysqli_query($db,"INSERT INTO Dibs(roomID,jsonData) values($i,'$bookings')");
+        }
+    }
+?>
